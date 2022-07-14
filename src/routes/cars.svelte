9e530e9a030268e99b0be09d11bbf89ec855f015
Thea Schöbl <dev@theaninova.de>
2022-07-14 19:28:11 +0200

    cars

--- src/routes/cars.svelte
+++ src/routes/cars.svelte
@@ -5,7 +5,17 @@
   import "swiper/css"
   import Car from "../lib/components/Car.svelte"
   import {groupNames} from "../lib/leaderboard/data/stage/group"
-  import {HashNavigation, Navigation, Parallax} from "swiper"
+  import {Controller, HashNavigation, Navigation, Parallax} from "swiper"
+
+  let controlledSwiper = null
+
+  const setControlledSwiper = e => {
+    const [swiper] = e.detail
+    // set Controller swiper instance
+    setTimeout(() => {
+      controlledSwiper = swiper
+    })
+  }
 
   const carSlides: {
     car: Car
@@ -21,24 +31,29 @@
 </script>
 
 <div class="swiper-container">
+  <img data-swiper-parallax="-23%" src="/processed/areas/japan-1920.webp" alt="background" />
+  <Swiper modules={[Controller]} on:swiper={setControlledSwiper} slidesPerView="auto">
+    {#each carSlides as { car, carIndex, groupName }}
+      <SwiperSlide no-content={carIndex !== 0}>
+        {#if carIndex === 0}
+          <h1>{groupName}</h1>
+        {/if}
+      </SwiperSlide>
+    {/each}
+  </Swiper>
   <Swiper
-    modules={[Parallax, Navigation, HashNavigation]}
+    modules={[Parallax, Navigation, HashNavigation, Controller]}
     navigation={true}
     parallax={false}
     centeredSlides={true}
     hashNavigation={{watchState: true}}
+    controller={{control: controlledSwiper}}
     slidesPerView="auto"
   >
-    <img
-      data-swiper-parallax="-23%"
-      slot="container-start"
-      src="/processed/areas/japan-1920.webp"
-      alt="background"
-    />
     {#each carSlides as { car, carIndex, groupName }}
-      <SwiperSlide data-hash={car.name.replace(/\s/g, "-")} is-title={carIndex === 0}>
+      <SwiperSlide data-hash={car.name.replace(/\s/g, "-")}>
         {#if carIndex === 0}
-          <h1>{groupName}</h1>
+          <!--<h1>{groupName}</h1>-->
         {/if}
         <Car {car} />
       </SwiperSlide>
@@ -70,7 +85,7 @@
   }
 
   .swiper-container {
-    display: flex;
+    position: relative;
     height: 100%;
 
     --swiper-navigation-color: #fff;
@@ -81,8 +96,8 @@
       height: 100%;
     }
 
-    :global(.swiper-slide[is-title="true"]) {
-      margin-left: 32px;
+    :global(.swiper-slide[no-content="true"]) {
+      width: 20px;
     }
 
     :global(.swiper-button-next),
