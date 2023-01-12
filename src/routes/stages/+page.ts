0aada65f6eb1b315bb659cf1fe1cd7e46b728996
Thea Schöbl <dev@theaninova.de>
2023-01-12 15:05:13 +0100

    feat: stage parsing

--- src/routes/stages/+page.ts
+++ src/routes/stages/+page.ts
@@ -2,7 +2,7 @@ import type {PageLoad} from "./$types"
 import type {Stage} from "../../../unity-parser/stage/stage"
 
 export const load = (async ({fetch}) => {
-  const stage = await fetch("/stages/Kenya1.json").then<Stage>(it => it.json())
+  const stage = await fetch("/stages/Indonesia2.json").then<Stage>(it => it.json())
   const normalMapUrl = await fetch(
     `/stages/terrain/${stage.staticObjects.terrain.children[0].name}.normal.png`,
   ).then(it => it.url)
